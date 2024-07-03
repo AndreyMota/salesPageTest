0c79b2fc4566b2a9c98c5183f7463b4d999fb67f
menos1 <m1@m1.168.1.10>
2024-07-03 15:01:23 -0300

    Tentei upar quiz1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SalePage />} />
-          <Route path='/quiz' element={<Quiz />} />
+          <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </BrowserRouter>
     </>
